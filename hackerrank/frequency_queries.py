8264121f4e7ffca3465ea2ebd67894564e21460d
Mateus Santiago <mateus.santiago01@gmail.com>
2021-03-22 16:01:09 -0300

    hackerrank: frequency_queries

---
+++ hackerrank/frequency_queries.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+
+def frequency_queries(queries):
+    memory = defaultdict(int)
+    frequency = defaultdict(int)
+    result = []
+
+    for op, elem in queries:
+        if op == 1:
+            if memory[elem] > 0 and frequency[memory[elem]] > 0: 
+                frequency[memory[elem]] -= 1
+            
+            memory[elem] += 1
+            frequency[memory[elem]] += 1
+
+        elif op == 2:
+            if memory[elem] > 0:
+                freq = memory[elem]
+                
+                if freq > 0:
+                    frequency[freq] -= 1
+                    frequency[freq - 1] += 1
+
+                memory[elem] -= 1
+        
+        else:
+            if frequency[elem] > 0:
+                result.append(1)
+
+            else:
+                result.append(0)
+
+    return result
